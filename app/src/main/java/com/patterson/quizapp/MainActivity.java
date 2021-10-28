76ef02193f41adec7afb225216771a48fe782556
Gary Arriaza <1333792@bcpss.org>
2021-10-28 15:27:50 -0400

    I'm working on my new project.

--- app/src/main/java/com/patterson/quizapp/MainActivity.java
+++ app/src/main/java/com/patterson/quizapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.patterson.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;   
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     //Declare variables
     Button answer1;
     Button answer2;
+    int score;
+    Button finishedBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         // initialize our variable & our IU elements
         answer1 = (Button) findViewById(R.id.answer1);
         answer2 = (Button) findViewById(R.id.answer2);
+        finishedBTN = (Button) findViewById(R.id.finishedBTN);
+        score = 0;
 
         // set button onClickListener
         answer1.setOnClickListener(new View.OnClickListener() {
@@ -45,8 +50,25 @@ public class MainActivity extends AppCompatActivity {
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
 
+                score = score + 1;
+
             }
         });
 
+        finishedBTN.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  String packageContext;
+                  Intent myIntent = new Intent(MainActivity.this, ScoreActivity.class);
+                  myIntent.putExtra( "score", score);
+                  startActivity(myIntent);
+              }
+        });
+
+
+
+
     }
 }
+
+
